a51a5a6cba2e0b3dc88b6c265d98c5a4bdfbcb70
Mitchell C. Foote <mcfoote321@gmail.com>
2024-02-04 01:20:29 -0500

    secondt question solution

---
+++ C06-Intent-Lab/C06-Intent-Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C06-Intent-Lab
+//
+//  Created by Mitch Foote on 2/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
